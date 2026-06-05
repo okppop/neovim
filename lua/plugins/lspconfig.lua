1658e7113f763f8790c45641484c805043d00c6e
okppop <okppop@protonmail.com>
2026-06-05 18:28:45 +0800

    feat: lsp add clangd

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -7,29 +7,29 @@ return {
 	},
 	config = function()
 		-- LSP keymaps
-        vim.api.nvim_create_autocmd('LspAttach', {
-            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-            callback = function(ev)
+		vim.api.nvim_create_autocmd('LspAttach', {
+		    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+		    callback = function(ev)
 				vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
-
-                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = ev.buf, desc = 'goto definition' })
-                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = ev.buf, desc = 'goto declaration' })
-                vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = ev.buf, desc = 'find references' })
-                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = ev.buf, desc = 'goto implementation' })
-                vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, { buffer = ev.buf, desc = 'rename symbol' })
-                vim.keymap.set({ 'n', 'v' }, '<Leader>ca', vim.lsp.buf.code_action, { buffer = ev.buf, desc = 'code action' })
-                vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, { buffer = ev.buf, desc = 'show diagnostic' })
-                vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { buffer = ev.buf, desc = 'previous diagnostic' })
-                vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { buffer = ev.buf, desc = 'next diagnostic' })
-                vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = ev.buf, desc = 'hover documentation' })
+	
+				vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = ev.buf, desc = 'goto definition' })
+				vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = ev.buf, desc = 'goto declaration' })
+				vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = ev.buf, desc = 'find references' })
+				vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = ev.buf, desc = 'goto implementation' })
+				vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, { buffer = ev.buf, desc = 'rename symbol' })
+				vim.keymap.set({ 'n', 'v' }, '<Leader>ca', vim.lsp.buf.code_action, { buffer = ev.buf, desc = 'code action' })
+				vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, { buffer = ev.buf, desc = 'show diagnostic' })
+				vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { buffer = ev.buf, desc = 'previous diagnostic' })
+				vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { buffer = ev.buf, desc = 'next diagnostic' })
+				vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = ev.buf, desc = 'hover documentation' })
 				vim.keymap.set('n', '<Leader>ct', function()
 					vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = ev.buf }), { bufnr = ev.buf })
 				end)
-            end,
-        })
+			end,
+		})
 		local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-		-- go
+	
+		-- gopls
 		vim.lsp.config.gopls = {
 			cmd = { 'gopls' },
 			filetypes = { 'go', 'gomod' },
@@ -64,8 +64,8 @@ return {
 			end,
 		})
 		vim.lsp.enable('gopls')
-
-		-- zig
+	
+		-- zls
 		vim.lsp.config.zls = {
 			cmd = { 'zls' },
 			filetypes = { 'zig' },
@@ -86,5 +86,37 @@ return {
 			end,
 		})
 		vim.lsp.enable('zls')
+	
+		-- clangd
+		vim.lsp.config.clangd = {
+			cmd = { 'clangd' },
+			filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'h', 'hpp' },
+			root_markers = { '.git', '.clangd', 'compile_commands.json' },
+			capabilities = capabilities,
+			settings = {
+				clangd = {
+					arguments = { '--background-index' },
+					inlayHints = {
+						enabled = true,
+						parameterNames = true,
+						deducedTypes = true,
+					},
+				},
+			},
+		}
+		vim.api.nvim_create_autocmd('BufWritePre', {
+			pattern = { "*.c", "*.cpp", "*.h", "*.hpp", "*.cc", "*.cxx", "*.hh" },
+			callback = function(ev)
+				vim.lsp.buf.format({
+					bufnr = ev.buf,
+					async = false,
+					timeout_ms = 1000,
+					filter = function(client)
+						return client.name == 'clangd'
+					end,
+				})
+			end,
+		})
+		vim.lsp.enable('clangd')
 	end,
 }
